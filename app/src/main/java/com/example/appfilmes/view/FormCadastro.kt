17b82e483bc378fcbbbae5cedc640d04332d2bfa
saviocunhaa <saviocunha.01@hotmail.com>
2023-12-09 17:45:18 -0300

    validacao de login e cadastro

--- app/src/main/java/com/example/appfilmes/view/FormCadastro.kt
+++ app/src/main/java/com/example/appfilmes/view/FormCadastro.kt
@@ -1,8 +1,11 @@
 package com.example.appfilmes.view
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.appfilmes.R
 import com.example.appfilmes.databinding.ActivityFormCadastroBinding
 import com.example.appfilmes.databinding.ActivityFormLoginBinding
@@ -18,14 +21,52 @@ class FormCadastro : AppCompatActivity() {
 
         binding.editEmail.requestFocus()
 
-        binding.btCadastrar.setOnClickListener {
+        binding.btVamosLa.setOnClickListener {
             val email = binding.editEmail.text.toString()
 
-            if (email.isEmpty()){
+            if (!email.isEmpty()){
+                binding.containerSenha.visibility = View.VISIBLE
+                binding.btVamosLa.visibility = View.GONE
+                binding.btContinuar.visibility = View.VISIBLE
+                binding.txtTitulo.setText("Saiba tudo sobre o mundo dos filmes.")
+                binding.txtDescricao.setText("Crie uma conta para saber mais")
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#FF018786")
+                binding.containerEmail.helperText = ""
+                binding.containerHeader.visibility = View.VISIBLE
+            } else{
                 binding.containerEmail.helperText = "O email é obrigatório!"
                 binding.containerEmail.boxStrokeColor = Color.parseColor("#FF0000")
             }
         }
+
+
+        binding.btContinuar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            if (!email.isEmpty() && !senha.isEmpty()){
+                Toast.makeText(this,"Cadastro realizado com sucesso", Toast.LENGTH_SHORT).show()
+            } else if (senha.isEmpty()){
+                binding.containerSenha.boxStrokeColor = Color.parseColor("#ff0000")
+                binding.containerSenha.helperText = "A senha é obrigatória"
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#ff018786")
+
+            }else if (email.isEmpty()){
+
+                binding.containerEmail.helperText = "O email é obrigatória"
+                binding.containerEmail.boxStrokeColor = Color.parseColor("#ff018786")
+            }
+        }
+
+
+        binding.bteEntrar.setOnClickListener {
+            val intent = Intent(this, FormLogin::class.java)
+            startActivity(intent)
+
+
+        }
+
+
 
 
 
